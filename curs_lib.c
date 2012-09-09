0bffd01d185220c2c30e630f8a5035f32ec17ee8
guns <self@sungpae.com>
2012-09-08 22:22:35 -0500

    Don't offer to exit on abort

--- curs_lib.c
+++ curs_lib.c
@@ -297,11 +297,6 @@ void mutt_query_exit (void)
   curs_set (1);
   if (Timeout)
     timeout (-1); /* restore blocking operation */
-  if (mutt_yesorno (_("Exit Mutt?"), M_YES) == M_YES)
-  {
-    endwin ();
-    exit (1);
-  }
   mutt_clear_error();
   mutt_curs_set (-1);
   SigInt = 0;
